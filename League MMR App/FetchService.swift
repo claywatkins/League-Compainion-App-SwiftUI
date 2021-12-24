57bfae694ff99c1a70331a345fe1d2b6de2af4f3
Clayton Watkins <watkinclayton17@yahoo.com>
2021-12-23 19:25:08 -0800

    Created function for getting MMR

--- League MMR App/FetchService.swift
+++ League MMR App/FetchService.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case noData
+    case badDecode
+    case tryAgain
+}
+
+class FetchService {
+    let baseURL = URL(string: "https://na.whatismymmr.com")!
+
+    func getMMR(summonerName: String, completion: @escaping (Result<MMR, NetworkError>) -> Void) {
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        urlComponents.path = "/api/v1/summoner"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "name", value: summonerName)
+        ]
+        let requestURL = urlComponents.url?.absoluteURL
+        var request = URLRequest(url: requestURL!)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print(error)
+                completion(.failure(.tryAgain))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let returnedMMRdata = try JSONDecoder().decode(MMR.self, from: data)
+                completion(.success(returnedMMRdata))
+            } catch {
+                print("Error decoding MMR data: \(error)")
+            }
+        }
+        task.resume()
+    }
+    
+}
