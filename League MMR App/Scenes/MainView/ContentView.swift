7e6a662b077300f375a0adae745f04385ac737d5
Clayton Watkins <watkinclayton17@yahoo.com>
2021-12-23 20:46:29 -0800

    Changed how textfield frame is rendered, adding spacing to vstack, created temp displays for normal and ranked mmr

--- League MMR App/Scenes/MainView/ContentView.swift
+++ League MMR App/Scenes/MainView/ContentView.swift
@@ -9,27 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State var summonerName = ""
-    @ObservedObject var vm: SummonerMMRViewModel = SummonerMMRViewModel()
-    
+    @ObservedObject var vm: SummonerMMRViewModel = SummonerMMRViewModel(fetchService: FetchService())
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 5){
+            VStack(spacing: 30) {
                 Text("Summoner Name:")
                     .padding(.top, 10)
                 TextField("Enter summoner name", text: $summonerName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 350, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                Button("Submit") {
+                    .frame(width: 350, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Button("Get My MMR!") {
                     print("Getting Summoner MMR")
+                    vm.summonerName = self.summonerName
+                    vm.getMMR()
                 }
+                Text("Normal MMR: \(vm.normalMMR)")
+                Text("Ranked MMR: \(vm.rankedMMR)")
+                Spacer()
             }
-            Spacer()
             .navigationBarTitle("What's my MMR?")
             .navigationBarItems(trailing: SettingsButton())
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
