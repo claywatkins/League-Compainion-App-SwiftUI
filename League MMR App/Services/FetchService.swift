9b78d41ca385a96d9efe9ec0cf51f004373eda77
Clayton Watkins <watkinclayton17@yahoo.com>
2021-12-23 20:45:26 -0800

    FetchService fetches MMR, sets properties

--- League MMR App/Services/FetchService.swift
+++ League MMR App/Services/FetchService.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class FetchService {
+    let apiInteraction = APIInteraction.shared
+    var normalMMR: Normal?
+    var rankedMMR: Ranked?
+    
+    func getMMR(for summoner: String, then: @escaping () -> Void) {
+        apiInteraction.getMMR(for: summoner) { result in
+            guard let mmr = try? result.get() else { return }
+            self.normalMMR = mmr.normal
+            self.rankedMMR = mmr.ranked
+            then()
+        }
+    }
     
 }
