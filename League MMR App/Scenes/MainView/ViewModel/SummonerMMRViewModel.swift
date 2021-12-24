5fa0358ea2d5bca67f60063da25e10d2b5cbb438
Clayton Watkins <watkinclayton17@yahoo.com>
2021-12-23 20:45:53 -0800

    ViewModel getsMMR, sets published objects when done

--- League MMR App/Scenes/MainView/ViewModel/SummonerMMRViewModel.swift
+++ League MMR App/Scenes/MainView/ViewModel/SummonerMMRViewModel.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class SummonerMMRViewModel: ObservableObject {
+    let fetchService: FetchService
     
+    @Published var summonerName: String = ""
+    @Published var normalMMR: Int = 0
+    @Published var rankedMMR: Int = 0
+    
+    init(fetchService: FetchService) {
+        self.fetchService = fetchService
+    }
+    
+    func getMMR() {
+        fetchService.getMMR(for: summonerName) {
+            DispatchQueue.main.async {
+                if let normalMMR = self.fetchService.normalMMR?.avg {
+                    self.normalMMR = normalMMR
+                    print("Done with normal!")
+                }
+                if let rankedMMR = self.fetchService.rankedMMR?.avg {
+                    self.rankedMMR = rankedMMR
+                    print("Done with ranked!")
+                }
+            }
+        }
+    }
 }
