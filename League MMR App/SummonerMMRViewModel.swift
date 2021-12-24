71faa300b862285b3de1a980f5788354a651a8eb
Clayton Watkins <watkinclayton17@yahoo.com>
2021-12-23 19:29:50 -0800

    Created SummonerMMRViewModel

---
+++ League MMR App/SummonerMMRViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SummonerMMRViewModel.swift
+//  League MMR App
+//
+//  Created by Clayton Watkins on 12/23/21.
+//
+
+import Foundation
+
+class SummonerMMRViewModel: ObservableObject {
+    
+}
